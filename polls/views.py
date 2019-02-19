58e7501ecd4a68a063e413bfddac7fae78d3bc0b
gao <gx1nice@163.com>
2019-02-19 16:11:17 +0800

    fix

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,6 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-
 		return Question.objects.order_by('-pub_date')[:5]
 
 
@@ -24,4 +23,20 @@ class DetailView(generic.DetailView):
 class ResultView(generic.DetailView):
 	models = Question
 	template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		data = {
+			'question': question,
+			'error_message': "You didn't select a choice."
+		}
+		return render(request, 'polls/detail.html', data)
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
