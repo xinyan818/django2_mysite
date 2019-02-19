e35eb600e1b44b488f858e2e797a717b7972009d
gao <gx1nice@163.com>
2019-02-19 18:41:34 +0800

    fix

--- polls/views.py
+++ polls/views.py
@@ -20,9 +20,10 @@ class DetailView(generic.DetailView):
 	template_name = 'polls/detail.html'
 
 
-class ResultView(generic.DetailView):
-	models = Question
+class ResultsView(generic.DetailView):
+	model = Question
 	template_name = 'polls/results.html'
+
 
 
 def vote(request, question_id):
